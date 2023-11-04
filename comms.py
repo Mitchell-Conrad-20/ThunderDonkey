866cd643b17aee35ed92c283b73ec453c34cd4a4
Mitchell Conrad <Mitchell.conrad@gmail.com>
2023-11-04 12:34:41 -0400

    thread??

--- comms.py
+++ comms.py
@@ -1,6 +1,6 @@
 import serial
 #import time
-#from threading import Thread
+from threading import Thread
 
 # Init UART on COM Port 3
 arduino = serial.Serial(port='COM3',  baudrate=115200, timeout=.1)
@@ -9,11 +9,17 @@ arduino = serial.Serial(port='COM3',  baudrate=115200, timeout=.1)
 
 # Read Comms Data
 def checkTrigger():
-    if(arduino.readline().decode() == "Trigger"):
-        return True
-    return False
+    while True:
+        if(arduino.readline().decode() == "Trigger"):
+            print("Trigger Detected")
 
-# Main Loop
-while True:
-    if(checkTrigger()):
-        print("Trigger Detected")
+
+
+if __name__ == "__main__":
+    # Setup
+    triggerThread = Thread(target=checkTrigger, daemon=True)
+    triggerThread.start
+
+    # Main Loop
+    while True:
+        pass
