d7663fffc1c76e26efc384240ea0326466210573
Mitchell Conrad <Mitchell.conrad@gmail.com>
2023-11-04 12:39:32 -0400

    cleaned up code

--- wiredESP/wiredESP.ino
+++ wiredESP/wiredESP.ino
@@ -7,33 +7,10 @@
 // Debounce Object
 Bounce trigger = Bounce();
 
-// // Pin Status
-// bool trigger = false;
-
-// // Debounce Timer
-// unsigned long debounceTimer = 0;
-
-// // Debounce Delay Time
-// unsigned int delayTime = 150;
-
-// // GPIO Interrupt Function
-// // Sends trigger pull signal to the PC
-// void IRAM_ATTR ISR() {
-//   if(millis() - debounceTimer > delayTime){
-//     debounceTimer = millis();
-//     trigger = true;
-//   }
-// }
-
-
 void setup() {
   // Initialize Serial Comms
   Serial.begin(115200);
   Serial.setTimeout(1);
-
-  // Setup Trigger Interrupt
-  // pinMode(18, INPUT_PULLDOWN);
-  // attachInterrupt(18, ISR, RISING);
 
   trigger.attach(TRIGGER_PIN, INPUT_PULLUP);
   trigger.interval(25);
@@ -53,14 +30,6 @@ void loop() {
     // Send trigger event to the PC
     Serial.print("Trigger");
   }
-
-  // if(trigger){
-  //   // Send trigger event to the PC
-  //   Serial.print("Trigger");
-
-  //   // Reset Trigger
-  //   trigger = false;
-  // }
 }
 
 // Computer Vision Detected a Bullet Firing, Fire the Bullet
