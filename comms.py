1d54861590f03e7d7ea6b6ffe9a7795d58077ec0
Mitchell Conrad <Mitchell.conrad@gmail.com>
2023-11-04 13:40:33 -0400

    comms with commented out timing

--- comms.py
+++ comms.py
@@ -1,24 +1,21 @@
 import serial
 import time
-#from threading import Thread
 
 # Init UART on COM Port 3
 arduino = serial.Serial(port='COM3',  baudrate=115200, timeout=.1)
 
-#arduino.write(bytes(x,  'utf-8'))
-
-timer = 0
+#timer = 0
 
 # Read Comms Data
 def checkTrigger():
     global timer
     val = arduino.readline().decode()
     if(val == "T"):
-        timer = round(time.time() * 1000)
+        #timer = round(time.time() * 1000)
         return True
     elif(val == "F"):
-        print(round(time.time() * 1000) - timer)
-        #print("Fired")
+        #print(round(time.time() * 1000) - timer)
+        print("Fired")
 
     return False
 
